90a33dfddf7f7af4fda0df422b27b4989be935fc
Maheraj Ahmed <ahmed.m@etoncollege.org.uk>
2019-11-05 12:18:02 +0000

    Initial commit of project files

---
+++ Standard Algorithms/main.swift
@@ -0,0 +1,19 @@
+//
+//  main.swift
+//  Standard Algorithms
+//
+//  Created by Ahmed, Maheraj (HWTA) on 05/11/2019.
+//  Copyright © 2019 Ahmed, Maheraj (HWTA). All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
+let name = "Maheraj"
+
+for i in 1...3 {
+    print(i)
+}
+
+print(name)
