7aeb85409b2453fdda11e986f6af9ccd815490e4
Maheraj Ahmed <ahmed.m@etoncollege.org.uk>
2019-11-07 18:00:39 +0000

    BubbleSort Code completed

---
+++ Standard Algorithms/Sorting.swift
@@ -0,0 +1,33 @@
+//
+//  Sorting.swift
+//  Standard Algorithms
+//
+//  Created by Ahmed, Maheraj (HWTA) on 05/11/2019.
+//  Copyright © 2019 Ahmed, Maheraj (HWTA). All rights reserved.
+//
+
+import Foundation
+
+class Sorting {
+    func bubbleSort(data: [Int]) -> [Int] {
+        var array =  data
+        for i in 0..<array.count {
+            for j in 0..<array.count-2 {
+                if array[j-1]>array[j] {
+                    let temp = array[j-1]
+                    array[j-1] = array[j]
+                    array[j] = temp
+                }
+                
+            }
+            
+        }
+       return array
+    }
+}
+
+//func MergeSort (data: [Int]) -> [Int] {
+//    let middleOflist = data.count/2
+//    var leftOfList = [(data[0..<middleOflist])]
+//    var rightOfList = [(data[middleOflist..<data.count])]
+//}
---
+++ StandardAlgorithmsTest/SortingTest.swift
@@ -0,0 +1,39 @@
+//
+//  SortingTest.swift
+//  StandardAlgorithmsTest
+//
+//  Created by Ahmed, Maheraj (HWTA) on 05/11/2019.
+//  Copyright © 2019 Ahmed, Maheraj (HWTA). All rights reserved.
+//
+
+import XCTest
+
+class SortingTest: XCTestCase {
+    
+    func testBubbleSortWithIntegerArrayReturnsSortedArray() {
+        //arrange
+        let data = [6,3,2,4]
+        let expected = [2,3,4,6]
+        let sorting = Sorting()
+        //act
+        let actual = sorting.bubbleSort(data:data)
+        //assert
+        XCTAssertEqual(actual, expected)
+    }
+    func testBubbleSortWithManyIntegerArraysReturnsSortedArrays() {
+        //arrange
+        let testCases = [(input: [10, 3, 30, 2, 5], expected: [2, 3, 5, 10, 30]),
+                         (input: [4, 3, 15, 2, 1, 6], expected: [1, 2, 3, 4, 6, 15])]
+        
+        let sorting = Sorting()
+        
+        //act
+        //assert
+        for testCase in testCases {
+            let actual = sorting.bubbleSort(data: testCase.input)
+            XCTAssertEqual(actual, testCase.expected)
+        }
+        
+
+    }
+}
